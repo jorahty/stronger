51e5ed02911b73a874bb672d4c4a1425df157098
James Tennant <jtennant@ucsc.edu>
2023-06-27 22:36:26 -0700

    Restore error handler

--- src/app.ts
+++ src/app.ts
@@ -18,13 +18,13 @@ app.use('/api/v0', router);
 // Serve swagger UI
 app.use('/api/v0/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
-//   res.status(err.status).json({
-//     message: err.message,
-//     errors: err.errors,
-//     status: err.status,
-//   });
-// };
-// app.use(errorHandler);
+const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
+  res.status(err.status).json({
+    message: err.message,
+    errors: err.errors,
+    status: err.status,
+  });
+};
+app.use(errorHandler);
 
 export default app;
