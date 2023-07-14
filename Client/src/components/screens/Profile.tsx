5d9eb440680541393e90e7c91bad0b90886c8f68
James Tennant <jtennant@ucsc.edu>
2023-07-13 23:31:05 -0700

    render website, bio in profile

--- Client/src/components/screens/Profile.tsx
+++ Client/src/components/screens/Profile.tsx
@@ -23,28 +23,37 @@ export default function Profile() {
           gap: 20,
           justifyContent: 'space-between',
         }}>
-        <View style={{ flexDirection: 'row', gap: 20 }}>
-          <Image
-            source={
-              selectedUserDetails.image
-                ? { uri: selectedUserDetails.image }
-                : PlaceholderImage
-            }
-            style={{ width: 160, height: 160, borderRadius: 10 }}
-          />
-          <View style={{ justifyContent: 'space-around' }}>
-            <Text style={styles.title}>{selectedUserDetails.name}</Text>
-            <View
-              style={{ flexDirection: 'row', alignItems: 'center', gap: 5 }}>
-              <Ionicons name="location-outline" size={24} color="black" />
-              <Text>{selectedUserDetails.location}</Text>
-            </View>
-            {selectedUserDetails.website && (
-              <View style={{ flexDirection: 'row' }}>
-                <Text>{selectedUserDetails.website}</Text>
+        <View style={{ gap: 20 }}>
+          <View style={{ flexDirection: 'row', gap: 20 }}>
+            <Image
+              source={
+                selectedUserDetails.image
+                  ? { uri: selectedUserDetails.image }
+                  : PlaceholderImage
+              }
+              style={{ width: 160, height: 160, borderRadius: 10 }}
+            />
+            <View style={{ justifyContent: 'space-around' }}>
+              <Text style={styles.title}>{selectedUserDetails.name}</Text>
+              <View
+                style={{ flexDirection: 'row', alignItems: 'center', gap: 5 }}>
+                <Ionicons name="location-outline" size={24} color="black" />
+                <Text>{selectedUserDetails.location}</Text>
               </View>
-            )}
+              {selectedUserDetails.website && (
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    gap: 5,
+                  }}>
+                  <Ionicons name="link-outline" size={24} color="black" />
+                  <Text>{selectedUserDetails.website}</Text>
+                </View>
+              )}
+            </View>
           </View>
+          <Text>{selectedUserDetails.bio}</Text>
         </View>
         {selectedUserDetails.username === loginResponse.user.username ? (
           <Button
